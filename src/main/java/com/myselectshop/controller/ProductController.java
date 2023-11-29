78b08b89a61828c2eb07174f63fb2b8097e399bd
jinyr1128 <jinyr1128@gmail.com>
2023-11-29 10:58:38 +0900

    회원별 상품 API

--- src/main/java/com/myselectshop/controller/ProductController.java
+++ src/main/java/com/myselectshop/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.myselectshop.dto.ProductMypriceRequestDto;
 import com.myselectshop.dto.ProductRequestDto;
 import com.myselectshop.dto.ProductResponseDto;
 import com.myselectshop.repository.ProductRepository;
+import com.myselectshop.security.UserDetailsImpl;
 import com.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +19,12 @@ public class ProductController {
 
     private final ProductService productService;
 
+    // 관심 상품 등록하기
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-        return productService.createProduct(requestDto);
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 응답 보내기
+        return productService.createProduct(requestDto, userDetails.getUser());
     }
-
     // 관심 상품 희망 최저가 등록하기
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
@@ -30,8 +33,13 @@ public class ProductController {
     }
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts() {
+    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기
-        return productService.getProducts();
+        return productService.getProducts(userDetails.getUser());
+    }
+    // 관리자 조회
+    @GetMapping("/admin/products")
+    public List<ProductResponseDto> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/myselectshop/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.myselectshop.repository;
 
 import com.myselectshop.entity.Product;
+import com.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findALlByUser(User user);
 }
