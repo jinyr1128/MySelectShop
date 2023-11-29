43dc4d546f5a6add7013f4c33d017d526fc0d151
jinyr1128 <jinyr1128@gmail.com>
2023-11-29 11:18:47 +0900

    상품페이징 및 정렬

--- src/main/java/com/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/myselectshop/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.myselectshop.repository;
 
 import com.myselectshop.entity.Product;
 import com.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> findALlByUser(User user);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllByUser(User user, Pageable pageable);
 }
