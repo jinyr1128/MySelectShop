a001c8de60eaa599130d3453048886691277ac08
jinyr1128 <jinyr1128@gmail.com>
2023-11-29 16:47:00 +0900

    관심상품 등록 기능 추가

--- src/main/java/com/myselectshop/controller/FolderController.java
+++ src/main/java/com/myselectshop/controller/FolderController.java
@@ -1,20 +1,22 @@
 package com.myselectshop.controller;
 
 import com.myselectshop.dto.FolderRequestDto;
+import com.myselectshop.dto.FolderResponseDto;
 import com.myselectshop.security.UserDetailsImpl;
 import com.myselectshop.service.FolderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class FolderController {
+
     private final FolderService folderService;
+
 
     @PostMapping("/folders")
     public void addFolders(@RequestBody FolderRequestDto folderRequestDto,
@@ -23,5 +25,10 @@ public class FolderController {
         List<String> folderNames = folderRequestDto.getFolderNames();
 
         folderService.addFolders(folderNames, userDetails.getUser());
+    }
+    // 회원이 등록한 모든 폴더 조회
+    @GetMapping("/folders")
+    public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return folderService.getFolders(userDetails.getUser());
     }
 }
---
+++ src/main/java/com/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,12 @@
+package com.myselectshop.repository;
+
+import com.myselectshop.entity.Folder;
+import com.myselectshop.entity.Product;
+import com.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+    Optional<ProductFolder> findByProductAndFolder(Product product, Folder folder);
+}
